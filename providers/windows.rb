f16dd3f560bcb4af8555e945c848e7a94b08eb95
Justin Schuhmann <justin.schuhmann@e-hps.com>
2014-08-20 14:21:09 -0400

    added info

--- providers/windows.rb
+++ providers/windows.rb
@@ -17,7 +17,7 @@ action :add do
   unless @current_resource.exists
     CreateService()
   else
-    Chef::Log.debug("#{@new_resource} service already exists - nothing to do")
+    Chef::Log.info("#{@new_resource} service already exists - nothing to do")
   end
 end
 
@@ -28,7 +28,7 @@ def load_current_resource
   @current_resource.description(@new_resource.description)
   cmd = "sc query \"#{@new_resource.service_name}\""
   
-  Chef::Log.debug(cmd)
+  Chef::Log.info(cmd)
   if ((shell_out(cmd)).exitstatus == 0)
     @current_resource.exists = true
   else
@@ -41,7 +41,7 @@ private
     cmd = "sc create \"#{@new_resource.service_name}\" binPath= \"#{@new_resource.path}\" start= auto"
     cmd << " DisplayName= \"#{@new_resource.description}\"" if @new_resource.description
 
-    Chef::Log.debug(cmd)
+    Chef::Log.info(cmd)
     converge_by("Create #{ @new_resource } cmd #{cmd}") do
       shell_out!(cmd)
     end
