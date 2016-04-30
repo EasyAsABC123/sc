b3267c3fd1699abd554715f38d056d13d03c87f9
Fred Reimer <Fred.Reimer@jmfamily.com>
2016-04-30 16:34:25 -0400

    Added maintener, source_url, Berksfile, and .gitignore to standardize and allow this to work with later versions of berks

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
 name             "sc"
+maintainer       "Justin Schuhmann"
 maintainer_email "Justin.Schuhmann@gmail.com"
 description      "Manages Windows Services with SC"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.0"
+source_url 'https://github.com/EasyAsABC123/sc'
+version          "2.0.1"
 supports         "windows"
 depends          "windows", ">= 1.2.6"
